3d0cda64f551926e35a3abe736cf17690b6a8440
RohitMore31 <rohitmore1848@gmail.com>
2023-12-31 10:40:52 +0530

    design for schedule

--- ScoreInfo_UI/src/app/Homepages/schedule/schedule.component.ts
+++ ScoreInfo_UI/src/app/Homepages/schedule/schedule.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+export interface Match {
+  name: string;
+  status: string; // 'recent' or 'upcoming'
+}
+
 @Component({
   selector: 'app-schedule',
   templateUrl: './schedule.component.html',
@@ -7,4 +12,9 @@ import { Component } from '@angular/core';
 })
 export class ScheduleComponent {
 
+  teams: Match[] = [
+    { name: 'Match 1', status: 'recent' },
+    { name: 'Match 2', status: 'upcoming' },
+    // Add more matches with their status
+  ];
 }
